0dc2a0081ee84381f36be915507f5cb90da3c32d
vasilispapadakis <70390861+vasilispapadakis@users.noreply.github.com>
2020-09-16 02:16:20 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,22 +2,22 @@
 # Reading train, test tables and merging them with their row subject
 
 training <- read.table(
-        "train/X_train.txt", header=FALSE, comment.char = "", quote="\"")
+        "UCI HAR Dataset/train/X_train.txt", header=FALSE, comment.char = "", quote="\"")
 
 training_subject <- read.table(
-        "train/subject_train.txt", header=FALSE, comment.char = "", quote="\"",col.names = "subject")
+        "UCI HAR Dataset/train/subject_train.txt", header=FALSE, comment.char = "", quote="\"",col.names = "subject")
 
 train_y <- read.table(
-        "train/y_train.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "labels")
+        "UCI HAR Dataset/train/y_train.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "labels")
 
 test <- read.table(
-        "test/X_test.txt", header=FALSE, comment.char = "", quote="\"")
+        "UCI HAR Dataset/test/X_test.txt", header=FALSE, comment.char = "", quote="\"")
 
 test_subject <- read.table(
-        "test/subject_test.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "subject")
+        "UCI HAR Dataset/test/subject_test.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "subject")
 
 test_y <- read.table(
-        "test/y_test.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "labels")
+        "UCI HAR Dataset/test/y_test.txt", header=FALSE, comment.char = "", quote="\"" , col.names = "labels")
 
 test <- cbind( test_subject , test_y , test )
 
@@ -39,7 +39,7 @@ rm(test , training)
 library(dplyr)
 
 activities <- read.table(
-        "activity_labels.txt", header=FALSE, comment.char = "", quote="\"")
+        "UCI HAR Dataset/activity_labels.txt", header=FALSE, comment.char = "", quote="\"")
 
 data <- merge(data,activities , by.x = "labels" , by.y = "V1" , all = TRUE , )
 data <- select(data , subject , labels , V2.y , everything() )
@@ -49,7 +49,7 @@ data <- select(data , subject , labels , V2.y , everything() )
 #Naming the Columns
 
 column_names <- read.table(
-        "features.txt", header=FALSE, comment.char = "", quote="\"" )
+        "UCI HAR Dataset/features.txt", header=FALSE, comment.char = "", quote="\"" )
 column_names <- column_names[,2]
 column_names <- c(names(data[1:2]), "activity" , column_names)
 column_names <- make.names(names=column_names, unique=TRUE, allow_ = TRUE)
@@ -73,4 +73,4 @@ columns <- names(data) ; columns <- columns[-(1:3)]
 
 mean <- summarize_at(data , .vars = columns ,.funs =  mean )
 
-write.csv(mean ,file = "Mean.csv" , row.names = FALSE) 
+write.table(mean ,file = "Mean.csv" , row.names = FALSE) 
